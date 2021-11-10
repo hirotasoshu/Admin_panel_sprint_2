32c4def6b07600174611164495a421e5c7f29488
Maksim Zayakin <rasetsu@list.ru>
2021-11-10 06:54:00 +0500

    Add health checks to urls

--- server/urls.py
+++ server/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from health_check import urls as health_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health/", include(health_urls)),
     path("api/", include("server.apps.movies.api.urls")),
 ]
 

703829008275ccb2ed4d625ffea17e872dec08c2
Maksim Zayakin <rasetsu@list.ru>
2021-11-10 06:53:08 +0500

    Fix urls in prod env

--- server/urls.py
+++ server/urls.py
@@ -1,9 +1,15 @@
-import debug_toolbar
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__debug__/", include(debug_toolbar.urls)),
     path("api/", include("server.apps.movies.api.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
